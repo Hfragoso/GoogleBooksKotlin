d224c1e8e4b78ea5536dadbbd6c4179f8475f3f2
Hfragoso <heberfm@hotmail.com>
2019-06-11 11:26:59 -0700

    Added TODO

--- app/src/main/java/com/example/googlebooks_kotlin/di/modules/RoomModule.kt
+++ app/src/main/java/com/example/googlebooks_kotlin/di/modules/RoomModule.kt
@@ -1,12 +1,6 @@
 package com.example.googlebooks_kotlin.di.modules
 
-import android.content.Context
-import androidx.room.Room
-import com.example.googlebooks_kotlin.database.BookDao
-import com.example.googlebooks_kotlin.database.BookRoomDatabase
-import com.example.googlebooks_kotlin.di.scopes.Activity
 import dagger.Module
-import dagger.Provides
 
 @Module
 class RoomModule {
